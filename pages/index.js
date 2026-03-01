51b807c9d2793b83d47ab361c88c54b0caa4e6de
EntreProMarket <EntreProMarket@gmail.com>
2026-02-28 22:37:42 -0500

    Add Home component to index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{fontFamily: "Arial", padding: 40}}>
+      <h1>Entre PRO Market 🚀</h1>
+      <p>Your app is successfully connected.</p>
+    </div>
+  );
+}
