32986c2643cca15f02d89f789d48a6787b0aa0d3
EntreProMarket <EntreProMarket@gmail.com>
2026-03-11 15:34:49 -0400

    Create _app.js. Push changes to Vercel.

---
+++ pages/_app.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import { supabase } from "../lib/supabaseClient";
+
+export default function MyApp({ Component, pageProps }) {
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    const getSession = async () => {
+      const { data } = await supabase.auth.getSession();
+      setSession(data.session);
+    };
+
+    getSession();
+
+    const { data: listener } = supabase.auth.onAuthStateChange(
+      (_event, session) => {
+        setSession(session);
+      }
+    );
+
+    return () => {
+      listener.subscription.unsubscribe();
+    };
+  }, []);
+
+  return <Component {...pageProps} session={session} />;
+}
