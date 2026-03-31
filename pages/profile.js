5bf5f5ef6d1fd0682d141b2d7d8fb5fe5bba7156
EntreProMarket <EntreProMarket@gmail.com>
2026-03-31 08:52:13 -0400

    Delete pages/profile.js

--- pages/profile.js
+++
@@ -1,9 +0,0 @@
-import DashboardLayout from "../components/DashboardLayout";
-
-export default function Profile() {
-  return (
-    <DashboardLayout>
-      <h1>Profile Page</h1>
-    </DashboardLayout>
-  );
-}
