1f57428f14c847ddd41a1476e3f82717804aff02
EntreProMarket <EntreProMarket@gmail.com>
2026-04-13 15:39:50 -0400

    Update DashboardLayout.js

--- components/DashboardLayout.js
+++ components/DashboardLayout.js
@@ -172,7 +172,7 @@ export default function DashboardLayout({ children }) {
         </div>
 
         {/* NAV LINKS */}
-        {navItem("🏡 Home", () => { window.location.href = "/home"; })}
+        {navItem("🏡 Home", () => { window.location.assign("/home"); })}
         {navItem("🏠 Dashboard", goToDashboard)}
         {navItem("👤 Profile", goToProfile)}
         {navItem("🛒 Marketplace", () => navigate("/marketplace"))}
