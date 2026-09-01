8077d3f73d9fbd2bdefa32204f7f72c961a4f666
EntreProMarket <EntreProMarket@gmail.com>
2026-09-01 18:27:57 -0400

    Change image fit style from 'cover' to 'contain'

--- pages/vendor/[handle].js
+++ pages/vendor/[handle].js
@@ -6,7 +6,8 @@ import { supabase } from "../../lib/supabaseClient";
 import { SocialLinks } from "../../components/SocialIcons";
 
 const thumbStyle = (w, h, radius = 12) => ({ width: w, height: h, borderRadius: radius, border: "1px solid #e5e7eb", overflow: "hidden", cursor: "pointer", flexShrink: 0, display: "block" });
-const thumbImg = { width: "100%", height: "100%", objectFit: "cover", display: "block" };
+const thumbImg = { width: "100%", height: "100%", objectFit: "contain", display: "block" };
+const logoThumbStyle = (w, h, radius = 12) => ({ width: w, height: h, borderRadius: radius, border: "1px solid #e5e7eb", overflow: "hidden", cursor: "pointer", flexShrink: 0, display: "block", backgroundColor: "#f5f5f5" });
 
 export default function VendorPublicProfile() {
   useInactivityLogout();
@@ -134,7 +135,7 @@ export default function VendorPublicProfile() {
       {activeTab === "profile" && (
         <>
           {vendor.logo_url && (
-            <div onClick={() => setSelectedImage(vendor.logo_url)} style={thumbStyle(160, 160, 12)}>
+            <div onClick={() => setSelectedImage(vendor.logo_url)} style={logoThumbStyle(160, 160, 12)}>
               <img src={vendor.logo_url} alt="logo" style={thumbImg} />
             </div>
           )}
