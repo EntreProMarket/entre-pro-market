95eef1c1f655586328a3b7dfbdc4dbfaaf935693
EntreProMarket <EntreProMarket@gmail.com>
2026-09-18 11:14:27 -0400

    Remove header comment from home.js
    Removed header comment from the home page component.

--- pages/home.js
+++ pages/home.js
@@ -84,7 +84,6 @@ export default function HomePage() {
   return (
     <div style={{ fontFamily: "sans-serif" }}>
     <div style={{ maxWidth: 900, margin: "0 auto" }}>
-      {/* HEADER */}
       <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", padding: "12px 20px", borderBottom: "1px solid #eee", backgroundColor: "white", position: "sticky", top: 0, zIndex: 10 }}>
         <img src="/logo-circle.png" alt="EntreProMarket" style={{ width: 110, height: 110, objectFit: "contain", borderRadius: "50%", flexShrink: 0 }} />
         <div style={{ display: "flex", flex: 1, marginLeft: 24, alignItems: "center", justifyContent: "space-between" }}>
